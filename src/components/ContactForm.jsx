22f5e2d3b3c031d56d68c54f829405499db9114e
BabaFatimaZahra <fz.baba22@gmail.com>
2025-01-02 02:02:20 +0100

    Updated

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
 
-function ContactForm({onSubmit}) {
-  const [nom, setNom] = useState('');
-  const [email, setEmail] = useState('');
-  const [numTel, setNumTel] = useState('');
+function ContactForm({initialData = {}, onSubmit}) {
+  // const [nom, setNom] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [numTel, setNumTel] = useState('');
+  const [contact, setContact] = useState({nom : '', email : '', numTel : '', ...initialData});
 
   const handleSubmit = (e) => {
     e.prevent.default();
-    onSubmit({nom, email, numTel});
+    if(!nom || !email || !numTel ) {
+      alert('Le champ ne peut pas être vide ou contenir uniquement des espaces.');
+      return;
+    } else {
+      onSubmit(contact);
+    }
   }
+
 
   return (
     <>
       <form>
-        <input type="text" value={nom} onChange={(e) => setNom(e.target.value)} placeholder='Nom'/>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>
-        <input type="tel" value={numTel} onChange={(e) => setNumTel(e.target.value)} placeholder='Numero de telephone'/>
+        <input type="text" value={nom} onChange={(e) => setContact({...contact, nom : e.target.value})} placeholder='Nom'/>
+        <input type="email" value={email} onChange={(e) => setContact({...contact, email : e.target.value})} placeholder='Email'/>
+        <input type="tel" value={numTel} onChange={(e) => setContact({...contact, numTel : e.target.value})} placeholder='Numero de telephone'/>
         <button onClick={handleSubmit}>Soumettre</button>
       </form>
     </> 
--- src/pages/EditContact.jsx
+++ src/pages/EditContact.jsx
@@ -1,12 +1,19 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import ContactForm from '../components/ContactForm';
 
 function EditContact() {
-  const {id} = useParams(); 
   const navigate = useNavigate();
+  const {id} = useParams(); 
+  const [contact, setContact] = useState(null);
+
+  useEffect(async () => {
+    const url = `/contacts/${id}`;
+    const resp = await axios.get(url);
+    setContact(resp.data);
+  }, [id])
 
   const updateContact = async (contact) => {
     const url = `/contacts/${id}`;
@@ -17,7 +24,7 @@ function EditContact() {
     
   return (
     <>
-      <ContactForm onSubmit={updateContact} />
+      <ContactForm initialData={contact} onSubmit={updateContact} />
     </>
   )
 }
